f6451b2d1f3e937f2429fd7f61659be35cf3ca29
DaniAm94 <dani.amico94@gmail.com>
2024-02-01 16:32:59 +0100

    added container

--- src/App.vue
+++ src/App.vue
@@ -9,20 +9,29 @@ export default {
 </script>
 
 <template>
-    <!-- Header WIP -->
-    <AppHeader />
+    <div class="container">
+        <AppHeader />
+    </div>
 </template>
 
 <style>
+/* Reset */
 * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
 }
 
+/* Generics */
 img {
     max-width: 100%;
     height: auto;
     display: block;
+}
+
+/* Utils */
+.container {
+    max-width: 1080px;
+    margin: 0 auto;
 }
 </style>
